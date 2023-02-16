c7048a89f98c05e26bf30f594230eee869363f90
Deivid Cezar <d.dcezarsilva@gmail.com>
2023-02-16 10:47:55 -0300

    Tarefa 16 - Projeto integrador 4

---
+++ src/main/java/com/generation/redesocial/repository/PostagemRepository.java
@@ -0,0 +1,16 @@
+package com.generation.redesocial.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.generation.redesocial.model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("caixatexto") String caixatexto);
+	
+}
---
+++ src/main/java/com/generation/redesocial/repository/TemaRepository.java
@@ -0,0 +1,16 @@
+package com.generation.redesocial.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.generation.redesocial.model.Tema;
+
+@Repository
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+	
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(@Param("assunto") String assunto);
+
+}
